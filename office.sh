b12b7b622c2769dad8c499dcb3ecb001690ddde9
The-Receiver <47209193+The-Receiver@users.noreply.github.com>
2019-07-29 23:11:43 +0630

    Update office.sh

--- office.sh
+++ office.sh
@@ -5,15 +5,15 @@
 
 
 #Installing the keyboard layout guide
-cd 
-curl -O http://www.unicodetoday.org/pyidaungsu_mm.png
+cd Desktop && curl -O http://www.unicodetoday.org/pyidaungsu_mm.png && cd
 
 #Installing Libre Office
 sudo apt install libreoffice
 
+#Installing zathura
+sudo apt install zathura && sudo apt install zathura-pdf-mupdf
+
 #Changing the keyboard layout with setxkbmap (untested)
 #setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll us,th
-
-
 
 
