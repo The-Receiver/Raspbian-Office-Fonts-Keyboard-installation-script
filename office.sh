f1670e5cb61ad805d7b826b22be93c5f7ff14e31
The-Receiver <47209193+The-Receiver@users.noreply.github.com>
2019-07-30 15:54:35 +0630

    Using printf instead of echo
     for consistent \n behavior

--- office.sh
+++ office.sh
@@ -10,27 +10,27 @@ curl -O www.unicodetoday.org/downloads/Pyidaungsu-2.5.2_Numbers.ttf
 }
 
 #Make directory ~/.fonts, download fonts, then update font cache with fc-cache
-echo "Installing fonts"
+printf "Installing fonts \n\n"
 sleep 1
 sudo mkdir ~/.fonts
 cd ~/.fonts && download_fonts && fc-cache -f -v
 
 #Install the keyboard layout guide to the desktop
-echo "Installing keyboard layout guide."
+printf "Installing keyboard layout guide. \n\n"
 sleep 1
 cd Desktop && curl -O http://www.unicodetoday.org/pyidaungsu_mm.png
 
 #Install Libre Office
-echo "Installing Libre Office"
+printf "Installing Libre Office \n\n"
 sleep 1
 sudo apt install -y libreoffice
 
 #Installing Zathura pdf reader
-echo "Installing Zathura"
+printf "Installing Zathura \n\n"
 sleep 1
 sudo apt install -y zathura && sudo apt install zathura-pdf-mupdf
 
 #Installing screenfetch
-echo "Installing screenfetch"
+printf "Installing screenfetch \n\n"
 sleep 1
 sudo apt install -y screenfetch
