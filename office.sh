db2b746ddf9147b585d34ce7b9c9b85831d68474
The-Receiver <47209193+The-Receiver@users.noreply.github.com>
2019-07-30 14:04:11 +0630

    Added echo statements & removed && from line 14
    Sudo mkdir will return a failure if the directory it's trying to create already exists, but we should install the fonts even if the .fonts folder already ecists

--- office.sh
+++ office.sh
@@ -10,15 +10,20 @@ curl -O www.unicodetoday.org/downloads/Pyidaungsu-2.5.2_Numbers.ttf
 }
 
 #Make directory ~/.fonts, downloading fonts, then update font cache with fc-cache
-sudo mkdir ~/.fonts && cd ~/.fonts && download_fonts && fc-cache -f -v
+echo "Installing fonts"
+sudo mkdir ~/.fonts
+cd ~/.fonts && download_fonts && fc-cache -f -v
 
 #Install the keyboard layout guide to the desktop
+echo "Installing keyboard layout guide."
 cd Desktop && curl -O http://www.unicodetoday.org/pyidaungsu_mm.png
 
 #Install Libre Office
+echo "Installing Libre Office"
 sudo apt install libreoffice
 
 #Installing Zathura pdf reader
+echo "Installing Zathura"
 sudo apt install zathura && sudo apt install zathura-pdf-mupdf
 
 #Changing the keyboard layout with setxkbmap (untested)
